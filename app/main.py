6b37b9542e622ca79cd399df454200bed86c9bae
Alex Walczak <awal@berkeley.edu>
2018-03-12 23:08:04 -0700

    removed extraneous imports

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash, request
-from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
+from wtforms import Form, TextField, StringField, validators
  
 # App config.
 app = Flask(__name__)
